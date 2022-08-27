562c734a8157a1c2ff6fb3d873cfed93a5f601e7
Andrew Marmaduke <marmaduke.andrew@gmail.com>
2022-08-27 12:47:30 -0500

    add basic dialogue system

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,73 @@ fn main() {
         .insert_resource(DialogueState(0, 0))
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_system_set(SystemSet::on_update(GameState::Dialogue).with_system(dialogue))
+        .add_system_set(SystemSet::on_enter(GameState::Dialogue)
+            .with_system(dialogue_setup))
+        .add_system_set(SystemSet::on_update(GameState::Dialogue)
+            .with_system(dialogue_text)
+            .with_system(dialogue_next))
+        .add_system_set(SystemSet::on_exit(GameState::Dialogue)
+            .with_system(dialogue_cleanup))
         .add_system(bevy::window::close_on_esc)
         .run();
 }
+
+#[derive(Component)]
+struct DialogueText(Handle<Font>);
+
+#[derive(Component)]
+struct DialogueScene;
+
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct DialogueState(u32, u32);
+
+impl DialogueState {
+    fn left_character_path(&self) -> &'static str {
+        let DialogueState(id, _) = self;
+        match id {
+            0 => "characters/witch.png",
+            _ => "characters/witch.png",
+        }
+    }
+
+    fn right_character_path(&self) -> &'static str {
+        let DialogueState(id, _) = self;
+        match id {
+            0 => "characters/woman_blue.png",
+            _ => "characters/woman_blue.png",
+        }
+    }
+
+    fn maximum(&self) -> u32 {
+        let DialogueState(id, _) = self;
+        match id {
+            0 => 3,
+            _ => 0,
+        }
+    }
+
+    fn text(&self) -> &'static str {
+        let DialogueState(id, frame) = self;
+        match (id, frame) {
+            (0, 0) => "Hi",
+            (0, 1) => "Hello!",
+            (0, 2) => "This is awkward",
+            (0, 3) => "Yeah it is.",
+            _ => "meow"
+        }
+    }
+
+    fn next(&mut self) -> bool {
+        let maximum = self.maximum();
+        let DialogueState(id, frame) = self;
+        if *frame < maximum{
+            *frame += 1;
+            false
+        } else {
+            true
+        }
+    }
+}
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 enum GameState {
@@ -28,10 +89,15 @@ enum GameState {
     Cooking,
 }
 
-#[derive(Component)]
-struct Character;
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(Camera2dBundle::default());
+}
 
-fn setup(mut commands: Commands, windows: Res<Windows>, asset_server: Res<AssetServer>) {
+fn dialogue_setup(mut commands: Commands
+    , windows: Res<Windows>
+    , asset_server: Res<AssetServer>
+    , state: Res<DialogueState>)
+{
     let window = windows.primary();
     let (width, height) = (window.width(), window.height());
     let scale = height/2160.;
@@ -39,27 +105,26 @@ fn setup(mut commands: Commands, windows: Res<Windows>, asset_server: Res<AssetS
         .with_scale((scale, scale, 1.).into());
     let right_character_place = Transform::from_xyz(width/2. - width/4., 0., 0.)
         .with_scale((scale, scale, 1.).into());
-
-    commands.spawn_bundle(Camera2dBundle::default());
     
     commands.spawn()
-        .insert(Character)
+        .insert(DialogueScene)
         .insert_bundle(SpriteBundle {
-            texture: asset_server.load("characters/witch.png"),
+            texture: asset_server.load(state.left_character_path()),
             transform: left_character_place,
             ..default()
         });
     
     commands.spawn()
+        .insert(DialogueScene)
         .insert_bundle(SpriteBundle {
-            texture: asset_server.load("characters/woman_blue.png"),
+            texture: asset_server.load(state.right_character_path()),
             transform: right_character_place,
             ..default()
         });
 
     let font = asset_server.load("fonts/FiraCode-Regular.ttf");
     let text_style = TextStyle {
-        font,
+        font: font.clone(),
         font_size: 18.,
         color: Color::WHITE
     };
@@ -70,19 +135,7 @@ fn setup(mut commands: Commands, windows: Res<Windows>, asset_server: Res<AssetS
     let text_bounds = Text2dBounds { 
         size: text_size.into()
     };
-    let text_data = r#"
-        I want to tell you a story.
-        It's not long, it's not short, but it's my story.
-        Listen closely young one, because I'm only going to tell you this story once.
-        What?
-        You don't want to hear it?
-        Wait really?
-        No, no come on, it's a good story!
-        I'll dial it back a little, come on, man!
-        You'll hear me out?
-        Excellent, fantastic!
-        Okay, so where was I...
-    "#;
+
     commands.spawn_bundle(SpriteBundle {
         sprite: Sprite {
             color:Color::rgba(0., 0., 0., 0.5),
@@ -91,15 +144,45 @@ fn setup(mut commands: Commands, windows: Res<Windows>, asset_server: Res<AssetS
         },
         transform: Transform::from_xyz(0., -height/4. - height/16., 1.),
         ..default()
-    });
+    }).insert(DialogueScene);
     commands.spawn_bundle(Text2dBundle {
-        text: Text::from_section(text_data, text_style).with_alignment(text_alignment),
+        text: Text::from_section(state.text(), text_style).with_alignment(text_alignment),
         text_2d_bounds: text_bounds,
         transform: text_place(2.),
         ..default()
-    });
+    }).insert(DialogueText(font))
+        .insert(DialogueScene);
 }
 
-fn dialogue(mut state: ResMut<DialogueState>, input: Res<Input<KeyCode>>) {
-    
+fn dialogue_text(mut text: Query<&mut Text, With<DialogueText>>
+    , dialogue_text: Query<&DialogueText>
+    , state: Res<DialogueState>)
+{
+    let DialogueText(font) = dialogue_text.single();
+    let text_style = TextStyle {
+        font: font.clone(),
+        font_size: 18.,
+        color: Color::WHITE
+    };
+    let text_alignment = TextAlignment::CENTER;
+    let mut text = text.single_mut();
+    *text.as_mut() = Text::from_section(state.text(), text_style).with_alignment(text_alignment);
+}
+
+fn dialogue_next(mut state: ResMut<DialogueState>
+    , buttons: Res<Input<MouseButton>>
+    , mut game_state: ResMut<State<GameState>>)
+{
+    if buttons.any_just_released([MouseButton::Left, MouseButton::Right]) {
+        let dialogue_finished = state.next();
+        if dialogue_finished {
+            game_state.set(GameState::Cooking).ok();
+        }
+    }
+}
+
+fn dialogue_cleanup(mut commands: Commands, entities: Query<(Entity, &DialogueScene)>) {
+    for (entity, _) in entities.iter() {
+        commands.entity(entity).despawn_recursive();
+    }
 }
